eca7f2f41a9d62e94c7fb9d318d5f1546e769098
NyundohPapah <kelvinjuma0@gmail.com>
2022-12-15 00:57:43 -0800

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * jack_bauer - prints 24 hours
+ */
+void jack_bauer(void)
+{
+	int j, k;
+
+	for (j = 0; j < 24; j++)
+	{
+		for (k = 0; k < 60; k++)
+		{
+			if (j < 10)
+			{
+				_putchar('0');
+				_putchar(j + '0')
+			}
+			else if (j >= 10)
+			{
+				_putchar((j / 10) + '0');
+				_putchar((j % 10) + '0');
+			}
+			if (k < 10)
+			{
+				_putchar(':');
+				_putchar('0');
+				_putchar(k);
+			}
+			else if (k > 10)
+			{
+				_putchar(':');
+				_putchar((j / 10) + '0');
+				_putchar((j % 10) + '0');
+			}
+		}
+	}
+}
