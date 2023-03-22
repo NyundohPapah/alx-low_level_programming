d4369f95f25af7dabb33b8aabfbfde0c836fc8c0
NyundohPapah <kelvinjuma0@gmail.com>
2023-03-22 04:37:06 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -4,35 +4,28 @@
  */
 void jack_bauer(void)
 {
-	int j, k;
+	int j, k, l, m;
 
-	for (j = 0; j < 24; j++)
+	for (j = 0; j <= 2; j++)
 	{
-		for (k = 0; k < 60; k++)
+		for (k = 0; k <= 9; k++)
 		{
-			_putchar('\n');
-			if (j < 10)
+			if ((j <= 1 && k <= 9) || (j <= 2 && k <= 3))
 			{
-				_putchar('0');
-				_putchar(j + '0');
-			}
-			else if (j >= 10)
-			{
-				_putchar((j / 10) + '0');
-				_putchar((j % 10) + '0');
-			}
-			if (k < 10)
-			{
-				_putchar(':');
-				_putchar('0');
-				_putchar(k + '0');
-			}
-			else if (k >= 10)
-			{
-				_putchar(':');
-				_putchar((k / 10) + '0');
-				_putchar((k % 10) + '0');
+				for (l = 0; l <= 5; l++)
+				{
+					for (m = 0; m <= 9; m++)
+					{
+						_putchar(j + '0');
+						_putchar(k + '0');
+						_putchar(58);
+						_putchar(l + '0');
+						_putchar(m + '0');
+						_putchar('\n');
+					}
+				}
 			}
 		}
 	}
 }
+						
