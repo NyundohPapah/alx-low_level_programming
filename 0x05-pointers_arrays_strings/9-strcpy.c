fe3f85c3a02f2de4ce758bb9246c7ebbfaafa34a
NyundohPapah <kelvinjuma0@gmail.com>
2023-04-05 04:15:52 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -4,13 +4,16 @@
  * _strcpy - Copy a string
  * @dest: Destination value
  * @src: source value
- * @Return: the pointer to dest
+ * Return: the pointer to dest
  */
 
 char *_strcpy(char *dest, char *src)
+
 {
 	int i;
+
 	for (i = 0; src[i] != '\0'; i++)
+
 	{
 		dest[i] = src[i];
 	}
